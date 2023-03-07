bfbc52728d9a8bb08c06b69236e7572db6114e89
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-03-07 14:51:21 -0800

    Add files via upload

---
+++ public/src/books.js
@@ -0,0 +1,92 @@
+/******************************************************************************************* */
+
+
+//inputs: array of author objects & id number
+//output: return author object with matching id
+function findAuthorById(authors, id) {
+  return authors.find(authorObj => authorObj.id === id)
+}
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+
+
+//inputs: array of book objects & string ID of single bookObj
+//output: return book object with matching ID
+function findBookById(books, id) {
+  return books.find(bookObj => bookObj.id === id)
+}
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+
+
+//input: array ob book objects
+//output: return two arrays nested within an array
+         //1st array: currently checked out
+         //2nd array: books that are currently returned
+
+function partitionBooksByBorrowedStatus(books) {
+  //create desired arrays
+  const checkedOut = [];
+  const availableBooks = [];
+  
+  //for/in loop that pushes objects into arrays
+  for(const bookObj in books){
+    const book = books[bookObj]
+    //turnary. 
+    //if returned = T/F, push into appropriate array
+    book.borrows[0].returned === false ? checkedOut.push(book) : availableBooks.push(book)
+  }
+  
+  //return nested arrays
+  return [checkedOut, availableBooks]
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+//inputs: book object, array of account objects
+//output: returns array of 10 or fewer account objects
+        //accounts are the accounts in 'borrows array'
+        //insert new property into account obj
+            //returned: true/false
+//
+
+function getBorrowersForBook(book, accounts) {
+  //create empty result array
+  let result = [];
+  
+  //access borrowers of books. limit to 10 using .filter()
+  let last10Borrows = book.borrows.filter((borrowObj, indx)=>{
+    return indx < 10
+  })
+  console.log(last10Borrows)
+  console.log(last10Borrows.returned)
+  
+  //loop through accounts using .forEach()
+  accounts.forEach(account => {
+    //within each account, loop through last10borrows
+    last10Borrows.forEach(borrower=>{
+      //if id's match
+      if(account.id === borrower.id){
+        //create 'returned' property and set to T/F
+        account.returned = borrower.returned
+        //push account object into result array
+        result.push(account)
+      }
+      return result
+    })
+  })
+
+  //return result
+  return result
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
---
+++ public/src/home.js
@@ -0,0 +1,151 @@
+/*---------------------------------------------------------------------------------------------------*/
+
+//set up helper function to get counts.
+function arrayTotal(arrayToBeCounted){
+  return arrayToBeCounted.length
+}
+
+//input: array of book objects
+//output: number, represents the # of book objects in the array
+function getTotalBooksCount(books) {
+  return arrayTotal(books)
+}
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+
+//input: array of account objects
+//output: # of account objects in the array
+function getTotalAccountsCount(accounts) {
+  return arrayTotal(accounts)
+}
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+
+
+//input: array of book objects
+//output: number of books currently checked out
+function getBooksBorrowedCount(books) {
+  //filter books array. target: "returned":false
+  let filtered = books.filter(bookObj => bookObj.borrows[0].returned === false);
+  
+  //count using helper function
+  return arrayTotal(filtered)
+}
+
+
+
+
+/*--------------------------------------------------------------------------------*/
+
+
+
+
+//input: array of book objects
+//output: array containing max. 5 objects that represents the most common genre's
+      //ordered. most common first
+      //objects will have name and count keys
+
+function getMostCommonGenres(books) {
+  //reduce books array into object genre's as keys
+  const genreObj = books.reduce((result, element)=>{
+    //if genre undefined. create key with value of 1
+    if(result[element.genre] === undefined){
+      result[element.genre] = 1
+    }else{
+      //if key already defined, +=1
+      result[element.genre] += 1
+    }
+    return result
+  },{})
+
+  //rework into array with desired format
+  const genreArr = [];
+  for(const genre in genreObj){
+    genreArr.push({
+      "name": genre,
+      "count": genreObj[genre]
+    })
+  }
+
+  //sort array by largest to smallest
+  genreArr.sort((elementA,elementB)=>{
+    return elementB.count - elementA.count
+  })
+  
+  //return with splice(first 5 elements)
+  return genreArr.splice(0,5)
+}
+
+// console.log(getMostCommonGenres(books))
+
+
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+
+//input: array of book objects
+//output: returns array of 5 most popular books objects
+        //objects= {name: "title", count: #borrowed}
+        //popularity represented by amount of times book is borrowed
+        //
+function getMostPopularBooks(books) {
+  //access book.title and book.borrows.length
+  let reformattedBooks = books.map((book)=>{
+    return book = {
+      "name": book.title,
+      "count": book.borrows.length
+    }
+  })
+
+  //sort, splice, and return
+  return reformattedBooks.sort((bookA, bookB) => {return bookB.count - bookA.count}).splice(0,5)
+}
+
+console.log(getMostPopularBooks(books))
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+//input: array of book objects, array of author objects
+//output: array containing 5 or fewer objects of most popular authors
+          //popularity: add up all borrows for all books by author
+          //object: {"name": `${firstname} ${lastname}`, "count": #totalBorrows}
+function getMostPopularAuthors(books, authors) {
+  //create function that loops through books and counts borrows for each author
+  function borrowCounter(booksArr, author){
+    let result = 0;
+    books.forEach(book => {
+      if(book.authorId === author.id){
+        return result += book.borrows.length
+      }
+    });
+    return result
+  }
+
+  //reformat authors: add count to authors, combine name properties, remove id
+  let authorsWithCount = authors.map((author) => {
+    return author = {
+      "name": `${author.name.first} ${author.name.last}`,
+      "count": borrowCounter(books, author)
+    }
+  })
+
+  //sort, splice, return
+  return authorsWithCount.sort((authorA, authorB)=>{return authorB.count - authorA.count}).splice(0,5)
+}
+
+
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
